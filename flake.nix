c5da254d0e567f2e7ee24427bfc7d2d3f86b6254
DestinyofYeet <olebend@gmail.com>
2024-08-09 00:23:52 +0200

    flake test

--- flake.nix
+++ flake.nix
@@ -6,12 +6,13 @@
   outputs = { self, nixpkgs }: {
     nixosConfiguration.default = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      hydraJobs = {
-        pkgs = with nixpkgs; [
-          hello
-          stalwart-mail
-        ];
-      };
-    }
+    };
+  };
+
+  hydraJobs = {
+    pkgs = with outputs.packages; [
+      hello
+      stalwart-mail
+    ];
   };
 }
