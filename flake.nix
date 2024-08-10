269902c99a7ba9135692b51e0eb5edd302b7911f
DestinyofYeet <olebend@gmail.com>
2024-08-10 23:34:35 +0200

    flake maybe

--- flake.nix
+++ flake.nix
@@ -3,16 +3,19 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    nixosConfiguration.default = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-    };
-  };
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
 
-  hydraJobs = {
-    pkgs = with outputs.packages; [
-      hello
-      stalwart-mail
+    mkBuild = packageNames: 
+        builtins.listToAttrs (map (name: { inherit name; value = pkgs.${name}; }) packageNames);  
+  in {
+    packages.${system} = mkBuild [
+      "surrealdb"
+      "hello"
+      "stalwart-mail"
+      "elasticsearch"
     ];
   };
 }
