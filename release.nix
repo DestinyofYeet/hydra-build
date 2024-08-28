d0151a090ffe987b4f7784c775380240519f0249
DestinyofYeet <olebend@gmail.com>
2024-08-28 14:46:27 +0200

    fixed indenting

--- release.nix
+++ release.nix
@@ -4,12 +4,12 @@ let
   mkBuild = pkgs : packageNames: 
         builtins.listToAttrs (map (name: { inherit name; value = pkgs.${name}; }) packageNames);  
 
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-      ]
-      (system: function (import <nixpkgs> { inherit system; config.allowUnfree = true; }));
+  forAllSystems = function:
+    nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-linux"
+    ]
+    (system: function (import <nixpkgs> { inherit system; config.allowUnfree = true; }));
 
 in {
   packages = forAllSystems (pkgs: {
